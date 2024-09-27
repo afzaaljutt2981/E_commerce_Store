fa2e1d09b9284af3c59f9e6520429d548e5eee7a
Muhammad Afzaal <118747946+afzaaljutt2981@users.noreply.github.com>
2024-09-27 04:51:21 -0700

    Update app_theme.dart

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -6,7 +6,7 @@ class AppTheme {
   static ThemeData lightAppTheme = ThemeData(
     elevatedButtonTheme: ElevatedButtonThemeData( 
       style: ElevatedButton.styleFrom( 
-        padding: const EdgeInsets.all(12), 
+        padding: const EdgeInsets.all(12),  
         backgroundColor: const Color(0xFFf16b26),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12), // <-- Radius
